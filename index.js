fff4854110b4001a40f22fb207811949127c0256
mounikagotham <mounika.gotham@nationwidedigital.co.uk>
2018-12-20 16:54:40 +0000

    adding the microservice-two metrics

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var appzip = require('appmetrics-zipkin')({
+    host: '51.141.44.34',
+    port: 9411,
+    serviceName:"microservice-one",
+    sampleRate: 1.0
+  });
+
+
 const server = require('./lib/server')
 const pkg = require ('./package.json')
 const PORT = process.env.PORT || 8001
